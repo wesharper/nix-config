bcd227ab23d16c071ef240312dd026bc79761f38
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-02-04 23:34:54 -0700

    update packages

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -111,11 +111,6 @@
 
     # windowManager.i3 = {
     #   enable = true;
-    #   extraPackages = with pkgs; [
-    #     dmenu # application launcher most people use
-    #     i3status # gives you the default i3 status bar
-    #     i3lock # default i3 screen locker
-    #   ];
     # };
 
     # Configure keymap in X11
